dd18a959aed4dd185b8f3da259b3cc5560da58ea
Vilius Pranckaitis <vpranckaitis@gmail.com>
2020-12-15 20:19:50 +0200

    Solution for day 15

---
+++ y2020-julia/src/day15.jl
@@ -0,0 +1,38 @@
+function read(filename)::Vector{Int}
+    return open(filename, "r") do f
+        map(x -> parse(Int, x), split(collect(eachline(f))[1], ","))
+    end
+end
+
+function solve(data, k)
+    next = 0
+    occur = Dict{Int, Int}()
+    for i in 1:length(data)
+        next = haskey(occur, data[i]) ? i - occur[data[i]] : 0
+        occur[data[i]] = i
+    end
+
+    for i in (length(data) + 1):(k - 1)
+        new_next = haskey(occur, next) ? i - occur[next] : 0
+        occur[next] = i
+        next = new_next
+    end
+    return next
+end
+
+function solve1(data)
+    return solve(data, 2020)
+end
+
+function solve2(data)
+    return solve(data, 30000000)
+end
+
+
+function main()
+    input = read("files/15.in")
+    println(solve1(input))
+    println(solve2(input))
+end
+
+main()
