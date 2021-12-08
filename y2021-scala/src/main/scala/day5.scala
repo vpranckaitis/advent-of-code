29572c0fc27ad903fb34abd4ba5b1dc1b91e1409
Vilius Pranckaitis <vpranckaitis@gmail.com>
2021-12-08 17:43:26 +0200

    Solution for day 6

--- y2021-scala/src/main/scala/day5.scala
+++ y2021-scala/src/main/scala/day5.scala
@@ -21,34 +21,34 @@ object day5 extends App:
   input.close()
 
 
-def part1(directions: Seq[(Point, Point)]): Int =
-  val lines = directions.filter { case (p1, p2) => p1.x == p2.x || p1.y == p2.y }
+  def part1(directions: Seq[(Point, Point)]): Int =
+    val lines = directions.filter { case (p1, p2) => p1.x == p2.x || p1.y == p2.y }
 
-  val pos = lines.flatMap { case (p1, p2) =>
-    for {
-      x <- min(p1.x, p2.x) to max(p1.x, p2.x)
-      y <- min(p1.y, p2.y) to max(p1.y, p2.y)
-    } yield (x, y)
-  }
+    val pos = lines.flatMap { case (p1, p2) =>
+      for {
+        x <- min(p1.x, p2.x) to max(p1.x, p2.x)
+        y <- min(p1.y, p2.y) to max(p1.y, p2.y)
+      } yield (x, y)
+    }
 
-  pos.groupBy(identity).count { case (_, v) => v.sizeIs > 1 }
+    pos.groupBy(identity).count { case (_, v) => v.sizeIs > 1 }
 
 
-def part2(directions: Seq[(Point, Point)]): Int =
-  val (lines, diagonals) = directions.partition { case (p1, p2) => p1.x == p2.x || p1.y == p2.y }
+  def part2(directions: Seq[(Point, Point)]): Int =
+    val (lines, diagonals) = directions.partition { case (p1, p2) => p1.x == p2.x || p1.y == p2.y }
 
-  val posLines = lines.flatMap { case (p1, p2) =>
-    for {
-      x <- min(p1.x, p2.x) to max(p1.x, p2.x)
-      y <- min(p1.y, p2.y) to max(p1.y, p2.y)
-    } yield (x, y)
-  }
+    val posLines = lines.flatMap { case (p1, p2) =>
+      for {
+        x <- min(p1.x, p2.x) to max(p1.x, p2.x)
+        y <- min(p1.y, p2.y) to max(p1.y, p2.y)
+      } yield (x, y)
+    }
 
-  val posDiagonals = diagonals.flatMap { case (p1, p2) =>
-    val (pFrom, pTo) = if p1.x < p2.x then (p1, p2) else (p2, p1)
-    val length = pTo.x - pFrom.x
-    val yDir = if pFrom.y < pTo.y then 1 else -1
-    for (i <- 0 to length) yield (pFrom.x + i, pFrom.y + i * yDir)
-  }
+    val posDiagonals = diagonals.flatMap { case (p1, p2) =>
+      val (pFrom, pTo) = if p1.x < p2.x then (p1, p2) else (p2, p1)
+      val length = pTo.x - pFrom.x
+      val yDir = if pFrom.y < pTo.y then 1 else -1
+      for (i <- 0 to length) yield (pFrom.x + i, pFrom.y + i * yDir)
+    }
 
-  (posLines ++ posDiagonals).groupBy(identity).count { case (_, v) => v.sizeIs > 1 }
+    (posLines ++ posDiagonals).groupBy(identity).count { case (_, v) => v.sizeIs > 1 }
